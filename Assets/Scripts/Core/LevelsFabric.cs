22f2272ba4dd226f4e1f5b509e1d2c37b1ea371a
Kotikov D <kotikov.d@panoramik.com>
2022-08-19 13:54:27 +0300

    Setup Birds Level

--- Assets/Scripts/Core/LevelsFabric.cs
+++ Assets/Scripts/Core/LevelsFabric.cs
@@ -32,9 +32,9 @@ namespace MoroshkovieKochki
         public void InitLevels()
         {
             _levelsQueue = new Queue<Func<GameLevelPresenter>>();
-            _levelsQueue.Enqueue(InitLevel<MushroomsGameLevel, MushroomsLevelPresenter>);
             _levelsQueue.Enqueue(InitLevel<BirdsGameLevel, BirdsLevelPresenter>);
             _levelsQueue.Enqueue(InitLevel<MosquitoGameLevel, MosquitoLevelPresenter>);
+            _levelsQueue.Enqueue(InitLevel<MushroomsGameLevel, MushroomsLevelPresenter>);
             _levelsQueue.Enqueue(InitLevel<FinishGameLevel, FinishGameLevelPresenter>);
             _levelsQueue.Enqueue(InitLevel<FootprintsGameLevel, FootprintsLevelPresenter>);
             _levelsQueue.Enqueue(InitLevel<BerriesGameLevel, BerriesLevelPresenter>);
