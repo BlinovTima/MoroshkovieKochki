fdc5da7d7e75f71f7f610bdc975e1a010dee8c52
Kotikov D <kotikov.d@panoramik.com>
2022-09-20 21:51:24 +0300

    Setup Strat game level

--- Assets/Scripts/Levels/GameLevelPresenter.cs
+++ Assets/Scripts/Levels/GameLevelPresenter.cs
@@ -56,6 +56,7 @@ namespace MoroshkovieKochki
 
         public async UniTask CompleteLevel()
         {
+            AudioManager.StopSpeech();
             _levelTaskPopupPresenter.HidePopup().Forget();
             await _popupPresenter.CloseCurrentPopup();
             await AudioManager.PlaySpeechAsync(_gameLevel.FinishLevelSpeech);
